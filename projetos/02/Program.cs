79a4129c4b1c6cb9f3117d8e9107a31b12b5c76e
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-02-17 10:13:27 -0300

    Initial commit

---
+++ projetos/02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Alan Ryan");
+            if (args.GetLength(0) > 0)
+            {
+                Console.Write(args.GetValue(0));
+            }
+
+        }
+    }
+}
---
+++ projetos/03_variaveis/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program {
+    static void Main() {
+
+        // Declaração de variáveis
+        byte n1 = 10; // 0 e 255
+        char letra = 'c';
+        float valor = 5.3f;
+        string nome = "Alan";
+        int num1 = 10, num2 = 2, res;
+
+        res = num1 * num2;
+
+        var aux = nome;
+
+        Console.WriteLine("Valor da variável: " + aux + "...");
+        Console.WriteLine("A multiplicação de " + num1 + " com " + num2 + " é igual a: " + res);
+    }
+}
