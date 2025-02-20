1c8161efaee861c3c77df27ed44f6f736c9ab4e4
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-02-20 00:07:51 -0300

    feat: Created new programs.

--- projetos/01_hello_world/obj/Debug/net9.0/01.AssemblyInfo.cs
+++ projetos/01_hello_world/obj/Debug/net9.0/01.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("03")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa3ef5f2ea3bb1fe63d293969ea580e550e5ddee")]
-[assembly: System.Reflection.AssemblyProductAttribute("03")]
-[assembly: System.Reflection.AssemblyTitleAttribute("03")]
+[assembly: System.Reflection.AssemblyProductAttribute("01")]
+[assembly: System.Reflection.AssemblyTitleAttribute("01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
