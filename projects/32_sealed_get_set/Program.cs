dd822fc019f2035945069a9656b6d6c72c949ad4
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-03-07 01:50:15 -0300

    feat: Created new programs, adding content and the base structure of the C Sharp language

---
+++ projects/32_sealed_get_set/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+sealed class TanqueAgua { // sealed: Não permite herança
+    private int capacidadeMax;
+
+    public int Capacidade {
+        get {
+            return capacidadeMax;
+        }
+        set {
+            if (value < 0) {
+                capacidadeMax = 0;
+            }
+            else if (value > 1000) { 
+                capacidadeMax = 1000;
+            } else {
+                capacidadeMax = value;
+            }
+        }
+    }
+
+    public TanqueAgua() {
+        Capacidade = 500; 
+    }
+}
+
+class Program {
+    static void Main() {
+
+        Console.Clear();
+        Console.WriteLine("================================");
+
+        TanqueAgua tanque = new TanqueAgua();
+
+        tanque.Capacidade = 800; 
+
+        Console.WriteLine("Capacidade do tanque: {0} litros", tanque.Capacidade);
+        Console.WriteLine("================================");
+    }
+}
---
+++ projects/33_class_indexers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+sealed class TemperaturaCasa { 
+    private int[] temperaturas = new int[5] {18, 22, 25, 20, 24};
+
+    public int this[int i] {
+        get {
+            return temperaturas[i];
+        }
+        set {
+            if (value < 10) { 
+                temperaturas[i] = 10;
+            }
+            else if (value > 35) { 
+                temperaturas[i] = 35;
+            } else {
+                temperaturas[i] = value;
+            }
+        }
+    }
+
+    public TemperaturaCasa() {
+    }
+}
+
+class Program {
+    static void Main() {
+
+        TemperaturaCasa casa = new TemperaturaCasa();
+
+        casa[4] = 30; 
+
+        Console.Clear();
+        Console.WriteLine("=============================");
+        Console.WriteLine("Temperatura do cômodo 4: {0}°C", casa[4]);
+        Console.WriteLine("=============================");
+    }
+}
