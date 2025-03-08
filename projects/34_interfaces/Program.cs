2d6b2c12a1abec19b5616cf9d2cd5146c580b5fb
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-03-08 00:53:30 -0300

    feat: Created new programs, adding content and the base structure of the C Sharp language

---
+++ projects/34_interfaces/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+public interface Guerreiro {
+    void preparar();
+    void descansar();
+    void status();
+}
+
+public interface Arqueiro {
+    void dispararFlecha();
+}
+
+class ArqueiroDeElite : Guerreiro, Arqueiro {
+    public bool pronto;
+    private int flechas;
+
+    public ArqueiroDeElite() {
+        setFlechas(30);
+        Console.WriteLine("Arqueiro de elite recrutado!");
+    }
+
+    public void setFlechas(int qtde) {
+        this.flechas = qtde;
+    }
+
+    public void preparar() {
+        this.pronto = true;
+        Console.WriteLine("O arqueiro está preparado para a batalha.");
+    }
+
+    public void descansar() {
+        this.pronto = false;
+        Console.WriteLine("O arqueiro descansou e guardou seu arco.");
+    }
+
+    public void dispararFlecha() {
+        if (flechas > 0) {
+            Console.WriteLine("O arqueiro dispara uma flecha certeira!");
+            flechas--;
+        } else {
+            Console.WriteLine("Sem flechas restantes!");
+        }
+    }
+
+    public void status() {
+        Console.WriteLine($"O arqueiro está { (pronto ? "pronto" : "descansando") } e tem {flechas} flechas restantes.");
+    }
+}
+
+class Program {
+    static void Main() {
+
+        Console.Clear();
+        Console.WriteLine("=============================");
+
+        ArqueiroDeElite arqueiro = new ArqueiroDeElite();
+        arqueiro.status();
+
+        Console.WriteLine("=============================");
+    }
+}
---
+++ projects/35_object_with_get_set_properties/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace object_with_get_set_properties
+{
+    internal class Pessoa
+    {
+        private int id;
+        private int idade;
+        private string nome;
+        private string celular;
+
+        public int Id { get { return id; } set { id = value; } }
+        public int Idade { get { return idade; } set { idade = value; } }
+        public string Nome { get { return nome; } set { nome = value; } }
+        public string Celular { get { return celular; } set { celular = value; } }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p;
+            p = new Pessoa();
+
+            p.Id = 1;
+            p.Nome = "Alan Ryan da Silva Domingues";
+            p.Idade = 20;
+            p.Celular = "15 99620-9364";
+
+            string idMensagem = "ID: ";
+            string nomeMensagem = "Estudante: ";
+            string idadeMensagem = "Idade: ";
+            string celularMensagem = "Telefone: ";
+
+            Console.Clear();
+            Console.WriteLine("==========================================================");
+            Console.WriteLine(idMensagem + p.Id + "  \n" + nomeMensagem + p.Nome + " \n" + idadeMensagem + p.Idade + " \n" + celularMensagem + p.Celular);
+            Console.WriteLine("==========================================================\n");
+        }
+    }
+}
