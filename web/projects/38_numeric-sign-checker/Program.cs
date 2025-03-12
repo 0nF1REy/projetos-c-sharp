a2e61a275010ca74f7e3c2e7dcf6e8bee0f80ee6
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-03-12 00:45:48 -0300

    feat: Created new program, adding content and the base structure of the C Sharp language

---
+++ web/projects/38_numeric-sign-checker/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Variáveis
+        int num;
+
+        // Entrada de dados
+        Console.WriteLine("==========================");
+        Console.WriteLine("Forneça um número inteiro: ");
+        num = Convert.ToInt16(Console.ReadLine()); // Converte de forma explícita string para int
+        Console.WriteLine("- - - - - - - -");
+
+        // Testa se o número é maior ou igual a 0
+        if (num >= 0)
+        {
+            Console.WriteLine("Número Positivo!");
+        }
+        else
+        {
+            Console.WriteLine("Número Negativo!");
+        }
+
+        Console.WriteLine("==========================");
+        Console.ReadKey();
+    }
+}
