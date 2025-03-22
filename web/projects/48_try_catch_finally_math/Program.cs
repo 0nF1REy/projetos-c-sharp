d4314f10c4dc5221cbaadb95d225b534c8fa76a7
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-03-22 10:21:46 -0300

    feat: Created new program, adding content and the base structure of the C Sharp language

---
+++ web/projects/48_try_catch_finally_math/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Area {
+    public static float Quad(float bas, float alt) {
+        if(bas == 0 || alt == 0) {
+            throw new Exception("Base ou altura não podem ser igual a 0");
+        }
+        return bas * alt;
+    }
+}
+
+class Program {
+    static void Main() {
+
+    float area = 0;
+
+
+    Console.Clear();
+    Console.WriteLine("=========");
+
+    try {
+       area = Area.Quad(10F,5F);
+       Console.WriteLine("Área do quad: {0}", area);
+
+    } catch(Exception e) { // catch (DivideByZeroException e) 
+        Console.WriteLine("Erro: {0}", e.Message);
+        Console.WriteLine("-------");
+        Console.WriteLine("Ex: {0}", e);
+        Console.WriteLine("-------");
+        Console.WriteLine("Ex: {0}", e.GetType());
+
+    } finally {
+        Console.WriteLine("= - = - = - = -");
+        Console.WriteLine("Fim do processo!");
+    }
+
+    Console.WriteLine("=========");
+    }
+}
