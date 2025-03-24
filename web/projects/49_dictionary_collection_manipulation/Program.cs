db8f5b6b933f5422b9cba6766d5b4b8db5891d6c
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-03-24 03:09:41 -0300

    feat: Created new programs, adding content and the base structure of the C Sharp language

---
+++ web/projects/49_dictionary_collection_manipulation/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+class Program {
+    static void Main() {
+        Console.Clear();
+        Console.WriteLine("=========");
+
+        Dictionary <int, string> veiculos = new Dictionary <int, string>();
+
+        veiculos.Add(10, "Carro");
+        veiculos.Add(5, "Avião");
+        veiculos.Add(0, "Navio");
+        veiculos.Add(20, "Moto");
+        veiculos.Add(15, "Patinete");
+
+        // =================
+        //  veiculos.Clear();
+        // =================
+        veiculos.Remove(20);
+        
+        Console.WriteLine("Tamanho do Dictionary: {0}", veiculos.Count);
+    
+        Console.WriteLine("----");
+        int chave = 20;
+       
+        if (veiculos.ContainsKey(chave)) { // ContainsKey: Verifica se a chave existe no Dictionary.
+            Console.WriteLine("A chave {0} está na coleção!", chave);
+        } else {
+            Console.WriteLine("A chave {0} não está na coleção!", chave);
+        }
+
+        veiculos[15]="Bicicleta"; // Foi mudado o valor da chave 15 para bicicleta.
+
+        Console.WriteLine("----");
+        string valor = "Bicicleta";
+       
+        if (veiculos.ContainsValue(valor)) { // ContainsKey: Verifica se o valor existe no Dictionary.
+            Console.WriteLine("O valor {0} está na coleção!", valor);
+        } else {
+            Console.WriteLine("O valor {0} não está na coleção!", valor);
+        }
+
+        Console.WriteLine("----");
+
+        // 01
+        Dictionary<int, string>.ValueCollection valores = veiculos.Values;
+        foreach(string v in valores) {
+            Console.WriteLine(v);
+        }
+
+        Console.WriteLine("----");
+
+        // 02
+        foreach(KeyValuePair<int, string> v in veiculos) {
+            Console.WriteLine("Chave: {0}, Valor: {1}", v.Key, v.Value);
+        }
+      
+        Console.WriteLine("=========");
+        Console.WriteLine("Limpando a tela em 3... 2... 1... ✨");
+        Thread.Sleep(3000);
+        Console.Clear();
+    }
+}
---
+++ web/projects/50_linkedlist_collection_manipulation/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+class Program {
+    static void Main() {
+        Console.Clear();
+        Console.WriteLine("=========");
+        LinkedList <string> transp = new LinkedList<string>();
+
+        transp.AddFirst("Carro"); // AddFirst: Adiciona no início da lista
+        transp.AddFirst("Avião");
+        transp.AddFirst("Navio");
+        transp.AddFirst("Motocicleta");
+
+        transp.AddLast("Bicicleta"); // AddLast: Adiciona no fim da lista
+
+        LinkedListNode <string> no;
+        no = transp.FindLast("Navio"); // Busca o último nó da lista que contém o valor especificado.
+        transp.AddAfter(no, "Patinete"); // AddAfter: Adiciona após
+        no = transp.FindLast("Carro");
+        transp.AddBefore(no, "Patins"); // AddBefore: Adiciona antes
+
+        // transp.Clear();
+
+        if(transp.Find("Carro") == null) {
+            Console.WriteLine("Elemento não encontrado");
+        } else {
+             Console.WriteLine("Elemento encontrado");
+        }
+
+        Console.WriteLine("----");
+
+        transp.Remove("Navio");
+        transp.RemoveFirst(); // Remove o primeiro elemento
+        transp.RemoveLast(); // Remove o último elemento
+
+        foreach(string t in transp) {
+            Console.WriteLine("Transporte: {0}", t);
+        }
+        
+        Console.WriteLine("=========");
+        Console.WriteLine("Limpando a tela em 3... 2... 1... ✨");
+        Thread.Sleep(3000);
+        Console.Clear();
+    }
+}
