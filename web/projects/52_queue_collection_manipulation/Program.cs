a098f3ed2f141bcdf9c9bdc00f97ab5213a6f001
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-03-26 03:21:39 -0300

    feat: Created new program, adding content and the base structure of the C Sharp language

---
+++ web/projects/52_queue_collection_manipulation/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+class Program {
+    static void Main() {
+
+        Console.Clear();
+        Console.WriteLine("=========");
+
+        // Fila de monstros na dungeon
+        Queue<string> monstros = new Queue<string>();
+
+        monstros.Enqueue("Dragon"); // Adiciona na fila
+        monstros.Enqueue("Troll");
+        monstros.Enqueue("Minotaur");
+        monstros.Enqueue("Demon");
+
+        string monstroProcurado = "Vampire";
+        if(monstros.Contains(monstroProcurado)) {
+            Console.WriteLine("Monstro " + monstroProcurado + " encontrado na fila da dungeon");
+        } else {
+            Console.WriteLine("Monstro " + monstroProcurado + " não está na fila da dungeon");
+        }
+
+        // monstros.Clear();
+        Console.WriteLine("----");
+        Console.WriteLine("Primeiro monstro na fila: " + monstros.Dequeue());
+        Console.WriteLine("----");
+        Console.WriteLine("Próximo monstro da fila: " + monstros.Peek());
+        Console.WriteLine("----");
+
+        foreach(string monstro in monstros) {
+            Console.WriteLine("Monstro: " + monstro);
+        }
+
+        Console.WriteLine("----");
+        Console.WriteLine("Tamanho da fila da dungeon: " + monstros.Count);
+        Console.WriteLine("----");
+
+        // Console.WriteLine(monstros[2]);
+
+        while(monstros.Count > 0) {
+            Console.WriteLine(monstros.Dequeue() + " derrotado!");
+        }
+
+        Console.WriteLine("----");
+        Console.WriteLine("Tamanho da fila da dungeon: " + monstros.Count);
+        
+        Console.WriteLine("=========");
+        Console.WriteLine("Limpando a tela em 3... 2... 1... ✨");
+        Thread.Sleep(3000);
+        Console.Clear();
+    }
+}
